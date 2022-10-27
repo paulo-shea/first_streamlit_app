39c557607d6706faeea2bd2b4e34c646e4d7be13
paul o'shea <116828156+paulo-shea@users.noreply.github.com>
2022-10-27 18:46:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.caption('Omega 3 & Blueberry Oatmeal')
-streamlit.caption('Kale, Spinach & Rocket Smoothie')
-streamlit.caption('Hard-Boiled Free0Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free0Range Egg')
