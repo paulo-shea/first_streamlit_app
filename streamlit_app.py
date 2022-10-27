ab54a5d655bca1f24568d97becd6a1f154ae5c3c
paul o'shea <116828156+paulo-shea@users.noreply.github.com>
2022-10-27 18:42:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
-streamlit.header('Breakfast Menu')
+streamlit.caption('Omega 3 & Blueberry Oatmeal')
